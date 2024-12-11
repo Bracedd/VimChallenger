82fe3661ba385b44dc26bcf91e5f40a490a042cb
braced <ytbraced@gmail.com>
2024-12-11 14:59:00 +0400

    Removed SCSS()

--- rollup.config.js
+++ rollup.config.js
@@ -72,7 +72,7 @@ export default {
 		// If we're building for production (npm run build
 		// instead of npm run dev), minify
 		production && terser(),
-		scss()
+		
 	],
 	watch: {
 		clearScreen: false
